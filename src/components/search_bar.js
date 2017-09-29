dc140d604ad72b249e9b257cf416dcbf8c90cd07
Nitish Kumar <nit4781@gmail.com>
2017-09-29 06:51:27 +0530

    update

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -11,8 +11,6 @@ class SearchBar extends Component {
     return (
       <div className ="search-bar">
          <input
-         type="text"
-         placeholder = "ClickmetoSearch"
          onChange={event => this.onInputChange(event.target.value)}
          value={this.state.term} />
          <button onClick={ () => this.props.onSearchTermChange(this.state.term)} >Click me to Search</button>
